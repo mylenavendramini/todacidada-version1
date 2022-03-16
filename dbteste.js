8db1022db6bc9f1aa37a966bc229df75fd59bb0c
mylenavendramini <myvendramini@gmail.com>
2022-03-16 18:37:01 -0300

    git ignore

--- dbteste.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV == "production") {
-  // in heroko
-  module.exports = {
-    mongoURI:
-      "mongodb+srv://mylenavendramini:997812220@cluster0.pihxa.mongodb.net/todacidada?retryWrites=true&w=majority",
-  };
-} else {
-  module.exports = { mongoURI: "mongodb://localhost/blogapp" };
-}
