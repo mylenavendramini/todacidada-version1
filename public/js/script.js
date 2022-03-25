5be6cefda595408cd8796cadab7bb57cfacfc634
mylenavendramini <myvendramini@gmail.com>
2022-03-25 12:45:26 +0100

    css course

--- public/js/script.js
+++ public/js/script.js
@@ -12,9 +12,9 @@ const btnsOpenModalSingUp = document.querySelector(".btn--show-modal--singup");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section2 = document.querySelector("#section--2");
 const nav = document.querySelector(".nav");
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
+const tabs = document.querySelectorAll(".courses__tab");
+const tabsContainer = document.querySelector(".courses__tab-container");
+const tabsContent = document.querySelectorAll(".courses__content");
 const loginToChange = document.querySelector(".btn--SignUpToChange");
 const signUpToChange = document.querySelector(".btn--LoginToChange");
 
@@ -100,25 +100,25 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 
 ///////////////////////////////////////
 // Tabbed component
-/*
+
 tabsContainer.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".operations__tab");
+  const clicked = e.target.closest(".courses__tab");
 
   // Guard clause
   if (!clicked) return;
 
   // Remove active classes
-  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
-  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+  tabs.forEach((t) => t.classList.remove("courses__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("courses__content--active"));
 
   // Activate tab
-  clicked.classList.add("operations__tab--active");
+  clicked.classList.add("courses__tab--active");
 
   // Activate content area
   document
-    .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add("operations__content--active");
-});*/
+    .querySelector(`.courses__content--${clicked.dataset.tab}`)
+    .classList.add("courses__content--active");
+});
 
 ///////////////////////////////////////
 // Menu fade animation
