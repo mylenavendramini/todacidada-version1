b944199c9849ed310fcc3d447f6ccd2b24f6e62e
mylenavendramini <myvendramini@gmail.com>
2022-03-29 13:54:10 +0200

    fixing

--- public/js/script.js
+++ public/js/script.js
@@ -163,25 +163,25 @@ nav.addEventListener("mouseout", handleHover.bind(1));
 
 ///////////////////////////////////////
 // Reveal sections
-const allSections = document.querySelectorAll(".section");
+// const allSections = document.querySelectorAll(".section");
 
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
 
-  if (!entry.isIntersecting) return;
+//   if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target);
-};
+//   entry.target.classList.remove("section--hidden");
+//   observer.unobserve(entry.target);
+// };
 
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.15,
+// });
 
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add("section--hidden");
-});
+// allSections.forEach(function (section) {
+//   sectionObserver.observe(section);
+//   section.classList.add("section--hidden");
+// });
 
 ///////////////////////////////////////
