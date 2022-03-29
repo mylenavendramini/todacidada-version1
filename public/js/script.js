b24b421bfa9db878fd3ccf08d74147e20db232a1
mylenavendramini <myvendramini@gmail.com>
2022-03-29 13:12:29 +0200

    fixing

--- public/js/script.js
+++ public/js/script.js
@@ -163,51 +163,25 @@ nav.addEventListener("mouseout", handleHover.bind(1));
 
 ///////////////////////////////////////
 // Reveal sections
-const allSections = document.querySelectorAll(".section");
+// const allSections = document.querySelectorAll(".section");
 
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
+// const revealSection = function (entries, observer) {
+//   const [entry] = entries;
 
-  if (!entry.isIntersecting) return;
+//   if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target);
-};
+//   entry.target.classList.remove("section--hidden");
+//   observer.unobserve(entry.target);
+// };
 
-const sectionObserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.15,
-});
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.15,
+// });
 
-allSections.forEach(function (section) {
-  sectionObserver.observe(section);
-  section.classList.add("section--hidden");
-});
-
-// Lazy loading images
-const imgTargets = document.querySelectorAll("img[data-src]");
-
-const loadImg = function (entries, observer) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-
-  // Replace src with data-src
-  entry.target.src = entry.target.dataset.src;
-
-  entry.target.addEventListener("load", function () {
-    entry.target.classList.remove("lazy-img");
-  });
-
-  observer.unobserve(entry.target);
-};
-
-const imgObserver = new IntersectionObserver(loadImg, {
-  root: null,
-  threshold: 0,
-  rootMargin: "200px",
-});
-
-imgTargets.forEach((img) => imgObserver.observe(img));
+// allSections.forEach(function (section) {
+//   sectionObserver.observe(section);
+//   section.classList.add("section--hidden");
+// });
 
 ///////////////////////////////////////
